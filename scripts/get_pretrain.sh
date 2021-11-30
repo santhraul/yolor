37ad28e8c6e4f925317d07ada7af5739518ca7f2
santhraul <63024253+santhraul@users.noreply.github.com>
2021-11-30 07:52:49 +0530

    added download link for yolo_csp_x weight link

--- scripts/get_pretrain.sh
+++ scripts/get_pretrain.sh
@@ -4,4 +4,7 @@ rm ./cookie
 
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1UflcHlN5ERPdhahMivQYCbWWw7d2wY7U" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1UflcHlN5ERPdhahMivQYCbWWw7d2wY7U" -o yolor_w6.pt
+
+curl -c ./cookie -s -L "https://drive.google.com/u/0/uc?id=1L29rfIPNH1n910qQClGftknWpTBgAv6c&export=download" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1UflcHlN5ERPdhahMivQYCbWWw7d2wY7U" -o yolor_w6.pt
 rm ./cookie
