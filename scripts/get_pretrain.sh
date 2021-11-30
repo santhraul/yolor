cf872653d91177fb3798a808399fa5d54c73f1ff
santhraul <63024253+santhraul@users.noreply.github.com>
2021-11-30 08:21:02 +0530

    yolo_csp_x weight link corrected

--- scripts/get_pretrain.sh
+++ scripts/get_pretrain.sh
@@ -5,6 +5,6 @@ rm ./cookie
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1UflcHlN5ERPdhahMivQYCbWWw7d2wY7U" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1UflcHlN5ERPdhahMivQYCbWWw7d2wY7U" -o yolor_w6.pt
 
-curl -c ./cookie -s -L "https://drive.google.com/u/0/uc?id=1L29rfIPNH1n910qQClGftknWpTBgAv6c&export=download" > /dev/null
+curl -c ./cookie -s -L "https://drive.google.com/uc?id=1L29rfIPNH1n910qQClGftknWpTBgAv6c&export=download" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1L29rfIPNH1n910qQClGftknWpTBgAv6c" -o yolor_csp_x.pt
 rm ./cookie
